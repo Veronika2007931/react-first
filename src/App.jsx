8cc84787140424f63b8b94d2bc958704919f6112
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-12-16 12:50:31 +0200

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 
 import './App.css';
 import painting from './components/painting.json'
+// import {colorPickerOptions} from './colorOptions'
 import { PaintingList } from './components/PaintitgList/PaintingList'
-
+// import { ColorPicker } from './components/ColorPicker/ColorPicker';
+// import {Alert} from './components/Alert/Alert.jsx'
 function App() {
   return (
     <>
        <PaintingList painting={painting} />
+    {/* <Alert text="Last warning!!" type="warning"/>
+    <Alert text="Everything is ok!" type="succes"/>
+    <Alert text="Everything fall :( " type="eror"/> */}
+       {/* < ColorPicker options = {colorPickerOptions}/> */}
     </>
   );
 }
 
 export default App;
+
--- src/components/Painting/painting.jsx
+++ src/components/Painting/painting.jsx
@@ -1,14 +1,15 @@
-    
+import s from './Painting.module.css'
     import PropTypes from 'prop-types';
     export const Painting = ({id, title, url, quantity, price, author}) => {
     return(
-        <li key={id}>  <div>
-        <img src={url} alt="" width="480" />
-        <h2>{title}</h2>
-        <p>Автор: <a href={author.tag}></a></p>
-        <p>Ціна: {price} грн</p>
-        <p>Доступність: {quantity < 5 ? 'закінчується' :` є в наявності ${quantity}`}  </p>
-        <button type="button">Додати в кошик</button>
+        <li className={s.list}  key={id}>  
+        <div className={s.paintingdiv}>
+        <img className={s.img} src={url} alt="" width="480" />
+        <h2 className={s.title}>{title}</h2>
+        <p className={s.discription}>Автор: <a href={author.tag}></a></p>
+        <p className={s.discription}>Ціна: {price} грн</p>
+        <p className={s.discription}>Доступність: {quantity < 5 ? 'закінчується' :` є в наявності ${quantity}`}  </p>
+        <button className={s.button} type="button">Додати в кошик</button>
       </div>
       </li>)
     
